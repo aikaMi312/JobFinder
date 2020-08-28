71707c9a8d021b1cb7aa1ea03ee8f7ce05d00dcf
Ainura <miaika4245@gmail.com>
2020-08-28 17:40:24 +0300

    new desing

--- main.js
+++ main.js
@@ -2,5 +2,16 @@
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })
+//fixed navbar
+window.onscroll = function() {myFunction()};
 
-  
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
